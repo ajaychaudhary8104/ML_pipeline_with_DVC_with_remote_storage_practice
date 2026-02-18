c9c3dca4f2b1d53da4257d9ef7b92befa33a163e
ajaychaudhary8955 <ajaychaudhary8104@gmail.com>
2026-02-18 20:17:06 +0530

    update DVC with params

--- src/model_building.py
+++ src/model_building.py
@@ -28,6 +28,22 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 def load_data(file_path: str) -> pd.DataFrame:
     """
@@ -102,12 +118,12 @@ def save_model(model, file_path: str) -> None:
 
 def main():
     try:
-    
+        params = load_params('params.yaml')['model_building']
         train_data = load_data('./data/processed/train_tfidf.csv')
         X_train = train_data.iloc[:, :-1].values
         y_train = train_data.iloc[:, -1].values
 
-        clf = train_model(X_train, y_train, params={'n_estimators': 100, 'random_state': 42})
+        clf = train_model(X_train, y_train, params)
         
         model_save_path = 'models/model.pkl'
         save_model(clf, model_save_path)
